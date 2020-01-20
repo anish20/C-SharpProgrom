b05a5f76e60769bedc54883029fba925927c25be
Dn Vidya <anish@ferventsoft.com>
2020-01-20 19:14:20 +0530

    Add successfully

---
+++ SqureShap.cs
@@ -0,0 +1,16 @@
+
+//P-1 Write a program and print 5 rows and 5 columns sape as squre
+
+class SqureShape{
+public static void Main(String[] args){
+int row,col=5;
+row=col;
+for(int i=1;i<=row;i++){
+	for(int j=1;j<=col;j++){
+	Console.Write(" * "); //Create Columns in same line and until loop terminate	
+	}
+	Console.WriteLine(" ");	//Each time create new Row Until look terminate
+				
+	}
+}
+}
