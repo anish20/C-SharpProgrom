1375b5dab33b8633ebbeb49bc65d4c469ef42aeb
Dn Vidya <anish@ferventsoft.com>
2020-01-20 19:56:00 +0530

    upated successfully

--- SumOfNatural.cs
+++ SumOfNatural.cs
@@ -1,4 +1,4 @@
-//Write a program in C# Sharp to display the first 10 natural numbers.
+//Write a C# Sharp program to find the sum of first 10 natural numbers.
 //Nutural number always start with 1
 public class SumOfNautural{
 	public static void Main(){
